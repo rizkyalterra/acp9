3f9fbd6a2c42ac2caa4a6c875d22db647396255c
Rizky <rizky.kurniawan@alterra.id>
2021-06-17 20:17:23 +0700

    re hello

--- main.go
+++ main.go
@@ -3,10 +3,5 @@ package main
 import "fmt"
 
 func main() {
-<<<<<<< Updated upstream
-	fmt.Println("Hello World 5 6")
-	fmt.Println("Hello")
-=======
-	fmt.Println("Hello World 5 6 7")
->>>>>>> Stashed changes
+	fmt.Println("Hello World")
 }
